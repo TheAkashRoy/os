37a36ba851c3da2a60c4c2fd1ce26e22b7252828
TheAkashRoy <9akash.roy@gmail.com>
2023-05-03 07:52:26 +0530

    .

--- paging2.c
+++ paging2.c
@@ -41,7 +41,7 @@ main() {
 
   scanf("%d %d %d", & x, & y, & offset);
 
-  if (x > np || y >= s[i] || offset >= ps)
+  if (x > np || y >= s[x] || offset >= ps)
     printf("\nInvalid Process or Page Number or offset");
 
   else {
@@ -50,3 +50,57 @@ main() {
 
   }
 }
+
+// #include<stdio.h>
+// void main()
+// {
+// int memsize=15;
+// int pagesize,nofpage;
+// int p[100];
+// int frameno,offset;
+// int logadd,phyadd;
+// int i;
+// int choice=0;
+// printf("\nYour memsize is %d ",memsize);
+// printf("\nEnter page size:");
+// scanf("%d",&pagesize);
+
+// nofpage=memsize/pagesize;
+
+// for(i=0;i<nofpage;i++)
+// {
+// printf("\nEnter the frame of page%d:",i+1);
+// scanf("%d",&p[i]);
+// }
+
+// do
+// {
+// printf("\nEnter a logical address:");
+// scanf("%d",&logadd);
+// frameno=logadd/pagesize;
+// offset=logadd%pagesize;
+// phyadd=(p[frameno]*pagesize)+offset;
+// printf("\nPhysical address is:%d",phyadd);
+// printf("\nDo you want to continue(1/0)?:");
+// scanf("%d",&choice);
+// }while(choice==1);
+// }
+
+// OUTPUT:
+// Your memsize is 15
+// Enter page size:5
+
+// Enter the frame of page1:2
+
+// Enter the frame of page2:4
+
+// Enter the frame of page3:7
+
+// Enter a logical address:3
+
+// Physical address is:13
+// Do you want to continue(1/0)?:1
+
+// Enter a logical address:1
+
+// Physical address is:11
